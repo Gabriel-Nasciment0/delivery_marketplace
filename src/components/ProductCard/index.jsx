974f20bdbadbb37d8b15b990e3ded164535c2832
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2026-05-04 19:21:32 -0300

    .

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,13 +1,18 @@
+import "./style.css"
+function ProductCard({ Product, onAdd }) {
+    return (
+        <div className="product-card">
+            <img
+                // src={Product.image}
+                alt={Product.name}
+            />
 
+            <h2>{Product.name}</h2>
+            <p>R$ {Product.price.toFixed(2)}</p>
 
-
-
-
-
-
-
-function ProductCard(){
-
+            <button onClick={onAdd}>Adicionar</button>
+        </div>
+    )
 }
 
-export default ProductCard;
+export default ProductCard
